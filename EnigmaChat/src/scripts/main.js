0153a5cc957fd2b6bdae4d4de4cbdaf780cd2a96
dalaiboada <dalaiboadajob@gmail.com>
2025-11-08 23:36:36 -0400

    feat: añadir estilos de componentes

---
+++ EnigmaChat/src/scripts/main.js
@@ -0,0 +1,6 @@
+import typeEffect from './ui/UserInput.js';
+import ScannerStatus from './ui/ScannerStatus.js';
+import updateSystemTime from './ui/SystemTime.js';
+import switchTab from './ui/Tab.js';
+import generateParticles from './ui/Particles.js';	
+
